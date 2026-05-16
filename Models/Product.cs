967e12465f46bde715f1882f891d4e14e02ad0e8
te28teght-glitch <te28teght@gmail.com>
2026-05-17 01:44:24 +0300

    add: Product.cs

---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+namespace ShoppingList.Mobile.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public bool IsPurchased { get; set; }
+        public string AddedBy { get; set; } = "";
+    }
+}
